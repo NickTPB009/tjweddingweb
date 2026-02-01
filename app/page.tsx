e1e24cc3bd926eeca31ceff2d4ad91a5718ea0f6
NickTPB009 <131839913+NickTPB009@users.noreply.github.com>
2026-02-01 19:36:14 +1100

    Fix image filename casing for tiles

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
       <Header />
 
       <main className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
-        {/* Hero 通常需要更大边距/更宽展示：如果 Hero 内部已经做了容器，就把下面这层删掉 */}
         <section className="py-8 sm:py-12 lg:py-16">
           <Hero />
         </section>
